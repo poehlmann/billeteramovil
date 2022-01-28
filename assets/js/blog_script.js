859a165f036bb6fd6f784069d8084ef1c388e51d
bruno.poehlmann1 <poehlmann0110F>
2022-01-28 10:10:57 -0400

    cashin

--- assets/js/blog_script.js
+++ assets/js/blog_script.js
@@ -1,3 +1,29 @@
+
+if (window.navigator.geolocation) {
+    // console.log(window.navigator.geolocation);
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+    function successCallback(datos) {
+        // console.log(datos);
+        var YOUR_LAT = datos.coords.latitude;
+        var YOUR_LON = datos.coords.longitude
+        var iframe_agentes = document.createElement('iframe');
+        iframe_agentes.width="100%";
+        iframe_agentes.height="400px";
+        iframe_agentes.setAttribute("src", 'https://www.google.com/maps/d/embed?mid=158KpL_Wj6wHN2wOnj7ZoJ1_6_znkYZRq&ehbc=2E312F&ll='+YOUR_LAT+'%2C'+YOUR_LON+'&z=13');
+        document.getElementById("soli_agentes_bcp_atm").appendChild(iframe_agentes);
+
+    }
+
+    function errorCallback(error) {
+        var iframe_agentes = document.createElement('iframe');
+        iframe_agentes.width="100%";
+        iframe_agentes.height="400px";
+        iframe_agentes.setAttribute("src", 'https://www.google.com/maps/d/embed?mid=158KpL_Wj6wHN2wOnj7ZoJ1_6_znkYZRq&ehbc=2E312F');
+        document.getElementById("soli_agentes_bcp_atm").appendChild(iframe_agentes);
+    }
+}
+
 // Check active classes
 var checkClass = function() {
     if ( $('.item').hasClass('hide') ) {
