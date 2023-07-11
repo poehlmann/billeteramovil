3619ef0f5edd0bdc746b755d28d3f990ba0242ca
Bruno Poehlmann <bruno.poehlmann@hotmail.com>
2023-07-11 10:37:25 -0400

    refinacion de toda la web

--- assets/js/sw-utils.js
+++ assets/js/sw-utils.js
@@ -1,17 +1,15 @@
 
 // Guardar  en el cache dinamico
 function actualizaCacheDinamico( dynamicCache, req, res ) {
-
-
     if ( res.ok ) {
-        return caches.open( dynamicCache ).then( cache => {
-            cache.put( req, res.clone() );
-            return res.clone();
-        });
+        if((res.request.url.indexOf('http') === 0)) {
+
+            return caches.open(dynamicCache).then(cache => {
+                cache.put(req, res.clone());
+                return res.clone();
+            });
+        }
     } else {
         return res;
     }
-
-
-
 }
