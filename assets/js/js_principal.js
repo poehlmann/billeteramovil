2bf702a0c058e782d8c2a6cb572d8738c5eb196c
Bruno Poehlmann <bruno.poehlmann@hotmail.com>
2023-07-26 18:24:23 -0400

    refinacion de toda la web

--- assets/js/js_principal.js
+++ assets/js/js_principal.js
@@ -1,38 +1,38 @@
-$(window).on('load',function(){
-    (function () {
-        const second = 1000,
-            minute = second * 60,
-            hour = minute * 60,
-            day = hour * 24;
-
-        //I'm adding this section so I don't have to keep updating this pen every year :-)
-        //remove this if you don't need it
-        let today = new Date(),
-            dd = String(today.getDate()).padStart(2, "0"),
-            mm = String(today.getMonth() + 1).padStart(2, "0"),
-            yyyy = today.getFullYear(),
-            nextYear = yyyy + 1,
-            dayMonth = "08/11/",
-            dia_inauguracion = dayMonth + yyyy;
-            today = mm + "/" + dd + "/" + yyyy;
-
-        if (today < dia_inauguracion) {
-            dia_inauguracion = dayMonth + nextYear;
-        }
-        //end
-        const countDown = new Date(dia_inauguracion).getTime(),
-            x = setInterval(function() {
-                const now = new Date().getTime(),
-                    distance = countDown - now;
-                //do something later when date is reached
-                if (distance > 0) {
-                    window.location.href = 'temporizador.html';
-                    clearInterval(x);
-                }
-                //seconds
-            }, 0)
-    }());
-});
+// $(window).on('load',function(){
+//     (function () {
+//         const second = 1000,
+//             minute = second * 60,
+//             hour = minute * 60,
+//             day = hour * 24;
+//
+//         //I'm adding this section so I don't have to keep updating this pen every year :-)
+//         //remove this if you don't need it
+//         let today = new Date(),
+//             dd = String(today.getDate()).padStart(2, "0"),
+//             mm = String(today.getMonth() + 1).padStart(2, "0"),
+//             yyyy = today.getFullYear(),
+//             nextYear = yyyy + 1,
+//             dayMonth = "08/11/",
+//             dia_inauguracion = dayMonth + yyyy;
+//             today = mm + "/" + dd + "/" + yyyy;
+//
+//         if (today < dia_inauguracion) {
+//             dia_inauguracion = dayMonth + nextYear;
+//         }
+//         //end
+//         const countDown = new Date(dia_inauguracion).getTime(),
+//             x = setInterval(function() {
+//                 const now = new Date().getTime(),
+//                     distance = countDown - now;
+//                 //do something later when date is reached
+//                 if (distance > 0) {
+//                     window.location.href = 'temporizador.html';
+//                     clearInterval(x);
+//                 }
+//                 //seconds
+//             }, 0)
+//     }());
+// });
 $(document).ready(function() {
 
     // fetch('http://reqres.in/api/users')
