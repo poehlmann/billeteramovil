aac42e1e3a48dbdd494abc2f3d8fc18ecf368ce5
BTBNET\T08428 <BPoehlmann@bancred.com.bo>
2024-01-18 16:30:37 -0400

    CDA

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('assets/js/sw-utils.js');
 
 
-const cache_name = 'YAPE_CACHE_v1.1';
+const cache_name = 'YAPE_CACHE_v1.2';
 const static_cache = 'static-v1';
 const dynamic_cache = 'dynamic-v1';
 const inmutable_cache = 'inmutable-v1';
