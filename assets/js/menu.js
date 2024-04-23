936fd7937c42196d63c2277f3cdf78a77d1d3d07
Bruno Poehlmann <bruno.poehlmann@hotmail.com>
2024-04-23 18:31:50 -0400

    correciones QA

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -165,8 +165,8 @@ function MakeSearch(){
     // Obtenga el nombre del proyecto con "/", como: / myproj
     let projectName=pathName.substring(0,pathName.substring(1).indexOf('/')+1);
     // obtuve http: // localhost: 8083 / myproj
-    let realPath=localhostPaht;
-    console.log("realPath:",realPath);
+    let realPath=localhostPaht+"/"+projectName;
+    console.log("realPath:",realPath+"/"+projectName);
     // if("https://www.yape.com.bo/index.html"== window.document.location.href || "http://127.0.0.1:5500/index.html" == window.document.location.href){
     //     window.location.replace("https://www.yape.com.bo/");
     // }
