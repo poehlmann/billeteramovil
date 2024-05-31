96aebc35c20463b9456a9e107d0256a2950db703
Bruno Poehlmann <bruno.poehlmann@hotmail.com>
2024-05-31 15:12:53 -0400

    webconfig

--- assets/js/js_principal.js
+++ assets/js/js_principal.js
@@ -67,7 +67,7 @@ if(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream){
 }else{
     if(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream){
         $('.input-search').css('top', '130%');
-    }if (/android/i.test(userAgent)) {
+    }if ( window.innerWidth <= 997 ) {
         $('.input-search').css('top', '180%');
     }
 }
